245587e7f995f19fee119efb222d7cb47a63bcc5
cyferorg <41696703+cyferorg@users.noreply.github.com>
2023-02-16 15:00:12 +0000

    updates

--- docs/nav.js
+++ docs/nav.js
@@ -1,31 +1,3 @@
 document.write('\
-    <div role="navigation" id="nav" class="nav-collapse">\
-      <ul>\
-        <li><a href="../index.html#introduction">Introduction</a></li>\
-        <li><a href="../index.html#examples">Examples</a></li>\
-        <li><a href="../index.html#instructions">How to use Zoetic</a></li>\
-        <li><a href="../firstproject/firstproject.html">Your first project</a></li>\
-		<li><a href="../crop/crop.html">Crop</a></li>\
-		<li><a href="../intermediates/intermediates.html">Generating the videos</a></li>\
-		<li><a href="../subprojects/subprojects.html">Sub-projects</a></li>\
-		<li><a href="../menucommands/menucommands.html">Menu commands</a></li>\
-		<li><a href="../settings/settings.html">Settings</a></li>\
-		<li><a href="../furtherdiscussion/furtherdiscussion.html">Further discussion</a></li>\
-		<li><a href="../layers/layers.html">Fun with layers</a></li>\
-		<li><a href="../posters/posters.html">Posters</a></li>\
-		<li><a href="../feedbackprivacy/feedbackprivacy.html">Feedback and Privacy Statement</a></li>\
-      </ul>\
-    </div>\
-	</script>\
 	<div role="main" class="main">\
-	<div id="head">\
-	<table style="width:100%; table-layout: fixed;">\
-	<tr>\
-	<th><p style="float:left; width:1%; text-align:left;"><a href="#nav" class="nav-toggle">Menu</a></p></th>\
-	<th><a href="https://play.google.com/store/apps/details?id=uk.cyferltd.zoeticpro"><img src="../imgs/zoetic2.png" width="128" height="64" alt="zoetic icon"></a></th>\
-	<th><p></p></th>\
-	</tr>\
-	</table>\
-	</div>\
-	<hr>\
 ');
